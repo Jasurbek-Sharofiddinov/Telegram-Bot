c978217b7634dc463d8d68d7a7881df7c1b7400a
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-07-25 02:21:35 +0500

    added wikitest.py file to our bot then we will write code on it

---
+++ wikipedia-bot/bot.py
@@ -0,0 +1,38 @@
+"""
+ 2This is a echo bot.
+ 3It echoes any incoming text messages.
+ 4"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1915844247:AAE0goOwa6aB-6HDRO9IkYeX7UlvgNKHnKk'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
