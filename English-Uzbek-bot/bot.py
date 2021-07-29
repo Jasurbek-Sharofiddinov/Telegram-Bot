819398a5ccf79cb0e39e7a62620a0d25a596f688
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-07-29 22:12:41 +0500

    ready

--- English-Uzbek-bot/bot.py
+++ English-Uzbek-bot/bot.py
@@ -1,12 +1,8 @@
-"""
-This is a echo bot.
-
-It echoes any incoming text messages.
-"""
-
 import logging
-
 from aiogram import Bot, Dispatcher, executor, types
+from oxfordApi import getDefinitions
+from googletrans import Translator
+translator = Translator()
 
 API_TOKEN = '1907317088:AAFwA3OMsTK5GqZFG7oJXl6-YMwlUPDr5S8'
 
@@ -18,21 +14,38 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-  """
-    await message.reply("Hi!\nThis bot is specially made for english dictionary\n\tWelcome to Dictionary Uz-Eng bot")
+	await message.reply(
+		"Hi!\nThis bot is specially made for english dictionary\n\tWelcome to Dictionary Uz-Eng bot\nDo you need any help /help")
 
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+	await message.reply(
+		"Thanks for choosing Dictionary uz-eng bot.\n-Enter words in uzbek to know english meaning\n-Enter english words to know the definition ")
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
+async def tarjimon(message: types.Message):
+    print(message)
+    lang = translator.detect(message.text).lang
+    if len(message.text.split()) > 2:
+        dest = 'uz' if lang == 'en' else 'en'
+        await message.reply(translator.translate(message.text, dest).text)
+    else:
+        if lang=='en':
+            word_id = message.text
+        else:
+            word_id = translator.translate(message.text, dest='en').text
 
-    await message.answer(message.text)
+        lookup = getDefinitions(word_id)
+        if lookup:
+            await message.reply(f"Word: {word_id} \nDefinitions:\n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
 
 
 if __name__ == '__main__':
